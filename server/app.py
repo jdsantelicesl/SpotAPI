4b50f5255bfd85a1c61f9cc64473217da176a06a
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-01-15 16:35:29 -0800

    deploy ready

--- server/app.py
+++ server/app.py
@@ -17,6 +17,7 @@ app = Flask(__name__)
 CORS(app)
 app.secret_key = os.getenv("SECRET_KEY")
 
+
 @app.before_request
 def before_request():
     previous_time = None
@@ -26,12 +27,12 @@ def before_request():
             data = json.load(json_file)
             previous_time = data["previous"]
         except:
-            previous_time = round(clock.hour + (clock.minute/60), 2)
+            previous_time = round(clock.hour + (clock.minute / 60), 2)
             with open("cleanup.json", "w") as write:
                 data = {"previous": previous_time}
                 json.dump(data, write, indent=4)
-    
-    current_time = round(clock.hour + (clock.minute/60), 2)
+
+    current_time = round(clock.hour + (clock.minute / 60), 2)
 
     if (current_time < previous_time) or (current_time - previous_time >= 1):
         with open("tokens.json", "w") as json_file:
@@ -40,7 +41,7 @@ def before_request():
 
         with open("cleanup.json", "w") as json_file:
             # Set current time as prev
-            data = {"previous":current_time}
+            data = {"previous": current_time}
             json.dump(data, json_file, indent=4)
 
 
@@ -60,18 +61,16 @@ def callback():
     token = data[1]
     print("firstState: ", state)
     print("firstToken: ", token)
-    
+
     data = {}
 
     with open("tokens.json", "r") as json_file:
         try:
-            data = json.load(json_file) #read past data as to not delete by overwrite
+            data = json.load(json_file)  # read past data as to not delete by overwrite
         except:
             data = {}
 
-    data[state] = {
-        "token": token
-    }
+    data[state] = {"token": token}
 
     with open("tokens.json", "w") as json_file:
         json.dump(data, json_file, indent=4)
@@ -98,4 +97,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8888, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
